734680346d97e7d32f9060c5eac980fe6b056713
Egor Ostroumov <araimo@yandex.ru>
2022-05-22 08:25:20 +0300

    серверная часть. Дописать.

--- app/server_app.py
+++ app/server_app.py
@@ -10,8 +10,101 @@ import logging
 import loging.server_conf_log
 from errors import IncorrectDataRecievedError
 from logging_deco import log
+from descriptors import Port
+from metaclasses import ServerMarker
 
 SERVER_LOGGER = logging.getLogger('server')
+
+@log
+def args_reader():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', default=SERVER_PORT, type=int, nargs='?')
+    parser.add_argument('-a', default=SERVER_IP, nargs='?')
+    namespace = parser.parse_args(sys.argv[1:])
+    serv_adress = namespace.a
+    serv_port = namespace.p
+
+    if not 1023 < serv_port < 65536:
+        SERVER_LOGGER.critical(f'Попытка запуска сервера с указанием неподходящего порта '
+                               f'{serv_port}. Допустимы адреса с 1024 до 65535.')
+        sys.exit(1)
+
+    return serv_adress, serv_port
+
+class Server(metaclass=ServerMarker):
+    port = Port()
+
+    def __init__(self, listen_adress, listen_port):
+        self.adress = listen_adress
+        self.port = listen_port
+        # список клиентов:
+        self.clients = []
+        # список сообщений на отправку
+        self.messages = []
+
+        # словарь с сопоставленными именами и их сокетами
+        self.names = dict()
+
+    def init_socket(self):
+        listen_address, listen_port = args_reader()
+
+        SERVER_LOGGER.info(
+            f'Запущен сервер, порт для подключений: {self.port}, '
+            f'адрес с которого принимаются подключения: {self.adress}. '
+            f'Если адрес не указан, принимаются соединения с любых адресов.')
+        # Готовим сокет
+        transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        transport.bind((self.adress, self.port))
+        transport.settimeout(0.5)
+        # слушаем сокет
+        self.sock = transport
+        self.sock.listen()
+
+    def main_loop(self):
+        #инициализируем сокет
+        self.init_socket()
+
+        while True:
+            # Ждём подключения, если таймаут вышел, ловим исключение.
+            try:
+                client, client_address = self.sock.accept()
+            except OSError:
+                pass
+            #72 строка
+            else:
+                SERVER_LOGGER.info(f'Установлено соедение с ПК {client_address}')
+                clients.append(client)
+
+            recv_data_lst = []
+            send_data_lst = []
+            err_lst = []
+            # Проверяем на наличие ждущих клиентов
+            try:
+                if clients:
+                    recv_data_lst, send_data_lst, err_lst = select.select(clients, clients, [], 0)
+            except OSError:
+                pass
+
+            # принимаем сообщения и если ошибка, исключаем клиента.
+            if recv_data_lst:
+                for client_with_message in recv_data_lst:
+                    try:
+                        process_client_message(recieve_message(client_with_message),
+                                               messages, client_with_message, clients, names)
+                    except Exception:
+                        SERVER_LOGGER.info(f'Клиент {client_with_message.getpeername()} '
+                                           f'отключился от сервера.')
+                        clients.remove(client_with_message)
+
+            # Если есть сообщения, обрабатываем каждое.
+            for i in messages:
+                try:
+                    process_message(i, names, send_data_lst)
+                except Exception:
+                    SERVER_LOGGER.info(f'Связь с клиентом с именем {i[DESTINATION]} была потеряна')
+                    clients.remove(names[i[DESTINATION]])
+                    del names[i[DESTINATION]]
+            messages.clear()
 
 
 @log
@@ -73,35 +166,9 @@ def process_message(message, names, listen_socks):
                             f'отправка сообщения невозможна')
 
 
-@log
-def args_reader():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-p', default=SERVER_PORT, type=int, nargs='?')
-    parser.add_argument('-a', default=SERVER_IP, nargs='?')
-    namespace = parser.parse_args(sys.argv[1:])
-    serv_adress = namespace.a
-    serv_port = namespace.p
-
-    if not 1023 < serv_port < 65536:
-        SERVER_LOGGER.critical(f'Попытка запуска сервера с указанием неподходящего порта '
-                               f'{serv_port}. Допустимы адреса с 1024 до 65535.')
-        sys.exit(1)
-
-    return serv_adress, serv_port
-
-
 def main():
     #
-    listen_address, listen_port = args_reader()
 
-    SERVER_LOGGER.info(
-        f'Запущен сервер, порт для подключений: {listen_port}, '
-        f'адрес с которого принимаются подключения: {listen_address}. '
-        f'Если адрес не указан, принимаются соединения с любых адресов.')
-    # Готовим сокет
-    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    transport.bind((listen_address, listen_port))
-    transport.settimeout(0.5)
 
     # список клиентов , очередь сообщений
     clients = []
@@ -113,46 +180,7 @@ def main():
     # Слушаем порт
     transport.listen(MAX_CONNECTIONS)
     # Основной цикл программы сервера
-    while True:
-        # Ждём подключения, если таймаут вышел, ловим исключение.
-        try:
-            client, client_address = transport.accept()
-        except OSError:
-            pass
-        else:
-            SERVER_LOGGER.info(f'Установлено соедение с ПК {client_address}')
-            clients.append(client)
 
-        recv_data_lst = []
-        send_data_lst = []
-        err_lst = []
-        # Проверяем на наличие ждущих клиентов
-        try:
-            if clients:
-                recv_data_lst, send_data_lst, err_lst = select.select(clients, clients, [], 0)
-        except OSError:
-            pass
-
-        # принимаем сообщения и если ошибка, исключаем клиента.
-        if recv_data_lst:
-            for client_with_message in recv_data_lst:
-                try:
-                    process_client_message(recieve_message(client_with_message),
-                                           messages, client_with_message, clients, names)
-                except Exception:
-                    SERVER_LOGGER.info(f'Клиент {client_with_message.getpeername()} '
-                                f'отключился от сервера.')
-                    clients.remove(client_with_message)
-
-        # Если есть сообщения, обрабатываем каждое.
-        for i in messages:
-            try:
-                process_message(i, names, send_data_lst)
-            except Exception:
-                SERVER_LOGGER.info(f'Связь с клиентом с именем {i[DESTINATION]} была потеряна')
-                clients.remove(names[i[DESTINATION]])
-                del names[i[DESTINATION]]
-        messages.clear()
 
 
 if __name__ == '__main__':
