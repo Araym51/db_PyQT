c500afa9e434670f9c2644d27e7417021869bb53
Egor_Ostroumov <araimo@yandex.ru>
2022-06-19 21:10:02 +0300

    проблема с импортами. попробовать на ubuntu.

---
+++ app/doc/source/index.rst
@@ -0,0 +1,24 @@
+.. messenger documentation master file, created by
+   sphinx-quickstart on Thu Jun 16 22:17:14 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to messenger's documentation!
+=====================================
+
+.. toctree::
+   :maxdepth: 3
+   :caption: Contents:
+
+   launcher
+   common
+   loging
+   client
+   server
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- app/server/main_window.py
+++ app/server/main_window.py
@@ -2,10 +2,10 @@ from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QLabel, QTableView
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 
-from app.server.add_user import RegisterUser
-from app.server.config_window import ConfigWindow
-from app.server.remove_user import DelUserDialog
-from app.server.stat_window import StatWindow
+from add_user import RegisterUser
+from config_window import ConfigWindow
+from remove_user import DelUserDialog
+from stat_window import StatWindow
 
 
 class MainWindow(QMainWindow):
