b714634fb49758c624b1f686d2c77a5a4d1e0415
Egor Ostroumov <araimo@yandex.ru>
2022-06-26 11:39:03 +0300

    в директории добавлены файлы __init__.py

---
+++ app/client_app/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='messenger',
+    version='0.0.1',
+    description='messenger_client_project',
+    author='Egor Ostroumov',
+    author_email='araimo@yandex.ru',
+    packages=find_packages(),
+    install_requires=[
+        'pycryptodome',
+        'PyQt5',
+        'sqlalchemy',
+        'pycryptodomex'
+    ]
+)
