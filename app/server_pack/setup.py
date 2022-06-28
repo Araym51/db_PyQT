164798814235458cf0c424d383dca1d91d905f2b
Egor_Ostroumov <araimo@yandex.ru>
2022-06-28 19:34:48 +0300

    все нужые пакеты дополнительно перемещены в папку ..._pack

--- app/server_pack/setup.py
+++ app/server_pack/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='messenger',
+    name='server_pyqt_chat_June_Araym51',
     version='0.0.1',
     description='messenger_server_project',
     author='Egor Ostroumov',
